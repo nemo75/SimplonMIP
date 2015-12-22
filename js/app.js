d6386ead8a527176ea1394b9371e3e6b0ae7a516
Robin <robin.sangla@live.fr>
2015-12-22 12:28:25 +0100

    Page trombinoscope

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 "use strict";
-var app = new senna.App();
-    app.setBasePath('/');
-    app.addSurfaces('content');
-    app.addRoutes(new senna.Route(/\w+\.html/, senna.HtmlScreen));
+$(document).ready(function(){
+
+/* Initialisation de senna */
+
+	var app = new senna.App();
+	    app.setBasePath('/');
+	    app.addSurfaces('content');
+	    app.addRoutes(new senna.Route(/\w+\.html/, senna.HtmlScreen));
+
+/* Récupération des informations pour le trombinoscope */
+
+	$.getJSON('https://s.idsympa.com/u-13.json', function(data){
+		var template = $("#template").html();
+		console.log(template)
+		var info = Mustache.to_html(template, data);
+		$('.perso').html(info);
+	});
+});
+	
+
+
