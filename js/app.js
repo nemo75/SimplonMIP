26d114675172756bc2bd93729ea3f8d6d8ec90a8
Robin <robin.sangla@live.fr>
2015-12-22 15:48:59 +0100

    Trombinoscope dynamique avec Mustache.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 "use strict";
 /* Initialisation de senna */
+$(document).ready(function(){
+
 var app = new senna.App();
     app.setBasePath('/');
     app.addSurfaces(['content', 'bandeau']);
     app.addRoutes(new senna.Route(/\w+\.html/, senna.HtmlScreen));
+    senna.Screen.prototype.load = function() {
 
-$(document).ready(function(){
-	
-/* Récupération des informations pour le trombinoscope */
+    	$.getJSON('https://smp.ovh/ppl.json', function(data){
+			var template = $("#template").html();
+			var info = Mustache.to_html(template, {users:data});
+			$('.carte').html(info);
+		});
 
-	$.getJSON('https://s.idsympa.com/u-13.json', function(data){
-		var template = $("#template").html();
-		console.log(template)
-		var info = Mustache.to_html(template, data);
-		$('.perso').html(info);
-	});
+    };
+
 });
