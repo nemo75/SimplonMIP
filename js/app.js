6ed4ae71f6aad2f8a5a4a722983f2b7e547db386
Floriane <floriane_grondin@hotmail.com>
2015-12-22 13:14:22 +0100

    Ajout des logo, bandeau mise en place dans index et maj de senna pour intégrer le bandeau

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 "use strict";
 var app = new senna.App();
     app.setBasePath('/');
-    app.addSurfaces('content');
+    app.addSurfaces(['content', 'bandeau']);
     app.addRoutes(new senna.Route(/\w+\.html/, senna.HtmlScreen));
