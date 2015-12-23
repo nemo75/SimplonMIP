b8045eac7d29270360fc21d657cba2df727177eb
Andrea <andrea.buisset@hotmail.fr>
2015-12-23 11:50:42 +0100

    Modification du footer + bandeau partenaire

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(function(){
 
 var app = new senna.App();
     app.setBasePath('/');
-    app.addSurfaces(['content', 'bandeau']);
+    app.addSurfaces(['content']);
     app.addRoutes(new senna.Route(/\w+\.html/, senna.HtmlScreen));
     senna.Screen.prototype.activate = function() {
 		$.getJSON('https://smp.ovh/ppl.json', function(data){
